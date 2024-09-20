1fb7865ca4c0eb6f5065fa5205577a8e138e0266
DaGreenBoi <138707101+dagreenboi@users.noreply.github.com>
2024-09-20 17:30:40 +0200

    Create color.js

---
+++ src/lib/color.js
@@ -0,0 +1,33 @@
+function darkenColour(hex, amount) {
+    hex = hex.replace(/^#/, '');
+
+    const bigint = parseInt(hex, 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+
+    const nr = Math.max(0, r) / amount;
+    const ng = Math.max(0, g) / amount;
+    const nb = Math.max(0, b) / amount;
+
+    const nh = `#${(nr << 16 | ng << 8 | nb).toString(16).padStart(6, '0')}`;
+
+    return nh;
+}
+
+function lightenColour(hex, amount) {
+    hex = hex.replace(/^#/, '');
+
+    const bigint = parseInt(hex, 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+
+    const nr = Math.min(255, r + (255 - r) / amount);
+    const ng = Math.min(255, g + (255 - g) / amount);
+    const nb = Math.min(255, b + (255 - b) / amount);
+
+    const nh = `#${(nr << 16 | ng << 8 | nb).toString(16).padStart(6, '0')}`;
+
+    return nh;
+}
