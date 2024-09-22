c65777cf3dafbfe1eb0ea97f5e505da514192aa4
DaGreenBoi <138707101+dagreenboi@users.noreply.github.com>
2024-09-22 14:24:21 +0200

    Update PostList.svelte

--- src/lib/PostList.svelte
+++ src/lib/PostList.svelte
@@ -415,7 +415,7 @@
 					class="emoji-panel-button"
 					title="Open Emoji Panel"
 					on:click|preventDefault={() => {
-                        modals.showModal(EmojiPanel);
+                        modals.showModal(EmojiPanel, postInput);
 					}}
                 >
                     <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
