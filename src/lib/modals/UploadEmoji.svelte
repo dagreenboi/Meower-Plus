757cc8e8e8aaf1b9b3753e4c2938397be5b8df0f
DaGreenBoi <138707101+dagreenboi@users.noreply.github.com>
2024-09-22 20:01:08 +0200

    Update UploadEmoji.svelte

--- src/lib/modals/UploadEmoji.svelte
+++ src/lib/modals/UploadEmoji.svelte
@@ -69,7 +69,7 @@ const apiResp = await fetch(`https://api.meower.org/chats/${$chat._id}/emojis/${
         body: JSON.stringify({ name }),
     });
 const cidx = $chats.indexOf($chat);
-$chat.emojis = [...$chat.emojis,{_id: emojiId, name, animated: img.name.endsWith(".gif")];
+$chat.emojis = [...$chat.emojis,{_id: emojiId, name, animated: img.name.endsWith(".gif")}];
 $chats[cidx] = $chat; 
 
                 modals.closeLastModal();
