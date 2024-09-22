f0e282efbca3f071e98fb8d57dcac2f91c890040
DaGreenBoi <138707101+dagreenboi@users.noreply.github.com>
2024-09-22 19:14:22 +0200

    Update UploadEmoji.svelte

--- src/lib/modals/UploadEmoji.svelte
+++ src/lib/modals/UploadEmoji.svelte
@@ -1,9 +1,9 @@
 <script>
-	import Modal from "../../Modal.svelte";
+	import Modal from "../Modal.svelte";
 
-	import {authHeader, chats} from "../../stores.js";
-	import {apiUrl} from "../../urls.js";
-	import * as modals from "../../modals.js";
+	import {authHeader, chats} from "../stores.js";
+	import {apiUrl} from "../urls.js";
+	import * as modals from "../modals.js";
 
 	import {goto, focus} from "@roxi/routify";
 
